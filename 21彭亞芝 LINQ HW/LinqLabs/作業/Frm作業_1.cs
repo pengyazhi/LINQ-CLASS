0de4abc8107bb1feb79f20e7f0470961685a67cb
sihyong <gr354517@gmail.com>
2023-06-17 13:35:11 +0800

    作業1完成

---
+++ 21彭亞芝 LINQ HW/LinqLabs/作業/Frm作業_1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace LinqLabs.作業
+{
+    public partial class Frm作業_1 : Form
+    {
+        
+        public Frm作業_1()
+        {
+            InitializeComponent();
+            FillData();
+            SelectOrderYear();
+
+        }
+
+        private void SelectOrderYear()
+        {
+            foreach (DataRow row in nwDataSet1.Orders)
+            {
+                DateTime dateTime = (DateTime)row["OrderDate"];
+                int year = dateTime.Year;
+                if (!comboBox1.Items.Contains(year))
+                {
+                    comboBox1.Items.Add(year);
+                }
+            }
+        }
+
+        private void FillData()
+        {
+            this.ordersTableAdapter1.Fill(this.nwDataSet1.Orders);
+            this.order_DetailsTableAdapter1.Fill(this.nwDataSet1.Order_Details);
+            this.productsTableAdapter1.Fill(this.nwDataSet1.Products);
+            
+        }
+
+        FileInfo[] files;
+        System.IO.DirectoryInfo dir;
+
+        void Load_C_Data()
+        {
+            dir = new System.IO.DirectoryInfo(@"C:\Windows");
+            files = dir.GetFiles();
+            dataGridView1.DataSource = files;
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Load_C_Data();
+            IEnumerable<FileInfo> q = from log in files
+                    where log.Extension == ".log"
+                    select log;
+            dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Load_C_Data();
+            IEnumerable<FileInfo> q = from ct in files
+                where ct.CreationTime.Year == 2022
+                select ct;
+            
+            dataGridView1.DataSource = q.ToList();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Load_C_Data();
+            IEnumerable<FileInfo> q =from large in files
+                   where large.Length >10000
+                   orderby large.Length descending
+                  select large;
+
+            dataGridView1.DataSource = q.ToList();
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IEnumerable<NWDataSet.OrdersRow> q = from or in this.nwDataSet1.Orders
+                    select or;
+            dataGridView1.DataSource = q.ToList();
+
+            IEnumerable<NWDataSet.Order_DetailsRow> a = from od in this.nwDataSet1.Order_Details select od;
+            dataGridView2.DataSource = a.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //顯示選中的年份的全部order
+            if (comboBox1.SelectedItem == null)
+                return;
+            int selectYear = (int)comboBox1.SelectedItem;
+
+            IEnumerable<NWDataSet.OrdersRow> q = from date in this.nwDataSet1.Orders
+                                                 where date.OrderDate.Year == selectYear
+                                                 select date;
+            dataGridView1.DataSource = q.ToList();
+            
+
+            //============================================================================
+
+            //顯示選中的年份的order的第一筆資料的ordder_details呈現在dataGridView2上
+
+            //使用List<>裝取每一row的資料
+            List<int> OrderIDLists = new List<int>();
+            //遍歷每一筆dataGridView1的OrderID放到OrderIDLists
+            foreach (DataGridViewRow dataRow in dataGridView1.Rows)
+            {
+                int orderID = (int)dataRow.Cells["OrderID"].Value;
+                OrderIDLists.Add(orderID);
+            }
+
+            IEnumerable<NWDataSet.Order_DetailsRow> qq = from row in nwDataSet1.Order_Details
+                                                         where OrderIDLists.Contains(row.OrderID)
+                                                         select row;
+            dataGridView2.DataSource = qq.ToList();
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+                int selectedOrderID = (int)selectedRow.Cells["OrderID"].Value;
+
+                IEnumerable<NWDataSet.Order_DetailsRow> q = from row in nwDataSet1.Order_Details
+                                                            where row.OrderID == selectedOrderID
+                                                            select row;
+
+                dataGridView2.DataSource = q.ToList();
+            }
+        }
+
+        //一頁顯示幾筆資料
+        int showRows = 0;
+        //當前頁數,0為第一頁
+        int cntPage = 0;
+        //判斷上或下一頁是否為第一次按
+        bool isFirstClick = true;
+
+        
+
+        private void NextPages_Click(object sender, EventArgs e)
+        {
+            
+            showRows = Convert.ToInt32(textBox1.Text);
+            
+            if (cntPage < 0)
+            {
+                cntPage = 0;
+            }
+           
+            if (!isFirstClick)
+            {
+                cntPage++;
+            }
+            else
+            {
+                isFirstClick = false;
+            }
+
+            IEnumerable<NWDataSet.ProductsRow> q = from p in this.nwDataSet1.Products.Skip(cntPage* showRows).Take(showRows)
+                    select p;
+            this.dataGridView2.DataSource = q.ToList();
+            
+        }
+
+        private void PreviousPages_Click(object sender, EventArgs e)
+        {
+            showRows = Convert.ToInt32(textBox1.Text);
+            
+            if (cntPage <0)
+            {
+                cntPage = 0;  
+            }
+            if (!isFirstClick)
+            {
+                cntPage--;
+            }
+            else
+            {
+                isFirstClick = false;
+            }
+
+            IEnumerable<NWDataSet.ProductsRow> q = from p in this.nwDataSet1.Products.Skip(cntPage * showRows).Take(showRows)
+                    select p;
+            this.dataGridView2.DataSource = q.ToList();
+            
+        }
+
+        
+    }
+}
---
+++ LINQ_Class_zhi/LinqLabs/1. FrmHelloLinq.cs
@@ -0,0 +1,208 @@
+﻿using LinqLabs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Starter
+{
+    public partial class FrmHelloLinq : Form
+    {
+        public FrmHelloLinq()
+        {
+            InitializeComponent();
+            this.productsTableAdapter1.Fill(this.nwDataSet1.Products);
+            this.ordersTableAdapter1.Fill(this.nwDataSet1.Orders);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("int [] nums --foreach");
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach(int n in nums)
+            {
+                listBox1.Items.Add(n);
+            }
+           
+            listBox1.Items.Add("int [] nums --IEnumerator");
+          
+            IEnumerator en = nums.GetEnumerator();
+            while(en.MoveNext())
+            {
+                listBox1.Items.Add(en.Current);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<int> list = new List<int>() { 10, 20, 30, 40, 50, 60, 70, 80 };
+            listBox1.Items.Add("List<int> list  --foreach");
+            foreach(int n in list)
+            {
+                listBox1.Items.Add(n);
+            }
+            listBox1.Items.Add("List<int> list --IEnumerator");
+
+            List<int>.Enumerator en = list.GetEnumerator();
+            while (en.MoveNext())
+            {
+                listBox1.Items.Add(en.Current);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // 嚴重性程式碼說明專案檔案行隱藏項目狀態錯誤  CS1579 因為 'int' 不包含 'GetEnumerator' 的公用執行個體或延伸模組定義，所以 foreach 陳述式無法在型別 'int' 的變數上運作 LinqLabs    C: \Users\User\Desktop\LinqLabs(StartUp)\LinqLabs\1.FrmHelloLinq.cs    54  作用中
+
+            //            int w = 100;
+            //            foreach(int n in w) {}
+            listBox1.Items.Add("字串 -- foreach迴圈");
+            string sz = "ascdfsf";
+            foreach(char c in sz)
+            {
+                listBox1.Items.Add(c);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("LINQ -- foreach 取偶數");
+            
+            //Step1 : define source
+            int[] nums = { 1,2,3,4,5,6,7,8,9,10};
+           
+            //Step2 : define query
+            IEnumerable<int> q = from n in nums 
+                                                       where n % 2 == 0 && n>5 && n <10  
+                                                       select n;
+            
+            //Step3 : execute query
+            foreach(int n in q)
+            {
+                listBox1.Items.Add(n);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("LINQ --IsEven() 取偶數");
+
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //執行順序是先跑foreach迴圈 到"in q"時才會去IsEven()判斷n是不是偶數
+            IEnumerable<int> q = from n in nums
+                                 where IsEven(n)
+                                 select n;
+
+            foreach (int n in q)
+            {
+                listBox1.Items.Add(n);
+            }
+        }
+
+        private bool IsEven(int n)
+        {
+            return (n % 2 == 0);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("LINQ --Point   IEnumerable可投射任意型別");
+
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            
+            IEnumerable<Point> q = from n in nums
+                                 where IsEven(n)
+                                 select new Point(n,n*n);
+
+            foreach (Point n in q)
+            {
+                listBox1.Items.Add(n);
+            }
+
+            List<Point> list = q.ToList();
+            
+            dataGridView1.DataSource  = list;
+            
+            chart1.DataSource = list;
+
+            //以下設置chart1的第一筆Series
+            
+            //Point的兩個點的屬性名稱為X,Y
+            //public int X{ get{return x;}set {x = value;}}
+            //public int Y{get {return y; }set{y = value;}}
+            chart1.Series[0].XValueMember = "X";
+            chart1.Series[0].YValueMembers = "Y";
+            chart1.Series[0].Color = Color.SeaGreen;
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] sz = { "apple", "Apple", "Apple123", "xxxApple","ddddd" };
+
+            IEnumerable<string> strings = from s in sz
+                                          //先把全部字串都變小寫,在尋找包含"apple"字串的,且字串長度大於5
+                                          where s.ToLower().Contains("apple") &&  s.Length >5
+                                          select s;
+            foreach(string s in strings)
+            {
+                listBox1.Items.Add(s);
+            }
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+           
+            string x = textBox1.Text;
+            IEnumerable<NWDataSet.ProductsRow> q = from p in this.nwDataSet1.Products
+                    where !p.IsUnitPriceNull() && p.UnitPrice > 30 && p.UnitPrice < 100 && p.ProductName.StartsWith($"{x}")
+                    select p;
+            
+
+          dataGridView1.DataSource = q.ToList();
+           
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+           IEnumerable<NWDataSet.OrdersRow> d = from o in this.nwDataSet1.Orders
+                    where o.OrderDate.Year == 1997
+                    select o;
+            this.bindingSource1.DataSource = d.ToList();
+            dataGridView1.DataSource = this.bindingSource1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //nums.Where().Select()
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo directoryInfo = new System.IO.DirectoryInfo(@"C:\Windows");
+            FileInfo[] files = directoryInfo.GetFiles();
+            
+            dataGridView1.DataSource = files;   
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button49_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
